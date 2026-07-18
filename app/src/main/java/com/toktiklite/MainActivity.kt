83b0f9eb2442fee37d1344b26cc4131366fbcfa9
obakha <omarbakha007@gmail.com>
2026-07-18 15:13:35 +0100

    Fix: Add launcher intent filter and improve URL interception

--- app/src/main/java/com/toktiklite/MainActivity.kt
+++ app/src/main/java/com/toktiklite/MainActivity.kt
@@ -6,7 +6,6 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
